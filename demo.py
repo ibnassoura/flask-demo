c3ba251583b187288c442b17168d6fc38fc6031f
IbrahimNassoura <ibnassoura@yahoo.com>
2018-03-04 10:26:23 +0400

    Files

--- demo.py
+++ demo.py
@@ -2,18 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-
-
 @app.route('/SayHello/<username>')
-
 def say_hello(username):
-    return "Hello" + username
-
+    return "Hello " + username
 
 if __name__ == '__main__':
     app.run()
